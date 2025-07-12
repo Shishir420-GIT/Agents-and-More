7dc64071b6a16a06077ba6b05ace148b0de1cae9
Shishir420-GIT <66838029+Shishir420-GIT@users.noreply.github.com>
2025-07-12 22:42:44 +0530

    updated Day 05 BasicAgent

---
+++ Day_05/basicAgent.py
@@ -0,0 +1,31 @@
+import os
+from dotenv import load_dotenv
+from typing import TypedDict, List
+from langchain_openai import ChatOpenAI
+from langchain_core.messages import HumanMessage
+from langgraph.graph import StateGraph, START, END
+
+# Load environment variables from .env file
+
+load_dotenv()
+
+class AgentState(TypedDict):
+    messages: List[HumanMessage]
+
+llm = ChatOpenAI(model="gpt-4o", openai_api_key=os.getenv("OPEN_AI_KEY"))
+
+def process(state: AgentState) -> AgentState:
+    response = llm.invoke(state["messages"])
+    print(f"\nAI: {response.content}")
+    return state
+
+graph = StateGraph(AgentState)
+graph.add_node("process", process)
+graph.add_edge(START, "process")
+graph.add_edge("process", END) 
+agent = graph.compile()
+
+user_input = input("Enter: ")
+while user_input != "exit":
+    agent.invoke({"messages": [HumanMessage(content=user_input)]})
+    user_input = input("Enter: ")
